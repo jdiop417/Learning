0c16f5b927c63cb4e6821feae9504a8f795ae9cb
chenyang <yang.chen@changhong.com>
2018-07-26 19:43:57 +0800

    提交代码

--- micro-weather-basic/src/main/java/org/learning/spring/weather/service/Impl/WeatherDataServiceImpl.java
+++ micro-weather-basic/src/main/java/org/learning/spring/weather/service/Impl/WeatherDataServiceImpl.java
@@ -3,16 +3,35 @@ package org.learning.spring.weather.service.Impl;
 import lombok.Data;
 import org.learning.spring.weather.service.WeatherDataService;
 import org.learning.spring.weather.vo.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @Data
 public class WeatherDataServiceImpl implements WeatherDataService {
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String WEATHER_API = "http://wthrcdn.etouch.cn/weather_mini";
+
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
-        return null;
+        String uri = WEATHER_API + "?city=" + cityId;
+        return doGetWeatherData(uri);
     }
+
 
     @Override
     public WeatherResponse getDataByCityName(String cityName) {
+        String uri = WEATHER_API + "?city=" + cityName;
+        return doGetWeatherData(uri);
+    }
+
+    private WeatherResponse doGetWeatherData(String uri) {
+        ResponseEntity<WeatherResponse> result = restTemplate.getForEntity(uri, WeatherResponse.class);
+        if (result.getStatusCodeValue() == HttpStatus.OK.value()) {
+            return result.getBody();
+        }
         return null;
     }
 }
