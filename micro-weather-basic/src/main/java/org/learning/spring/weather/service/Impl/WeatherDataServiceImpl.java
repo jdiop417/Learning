e433421f83acad34d5650246c85aba4af2aafdcf
chenyang <yang.chen@changhong.com>
2018-07-26 19:47:53 +0800

    提交代码

--- micro-weather-basic/src/main/java/org/learning/spring/weather/service/Impl/WeatherDataServiceImpl.java
+++ micro-weather-basic/src/main/java/org/learning/spring/weather/service/Impl/WeatherDataServiceImpl.java
@@ -1,14 +1,14 @@
 package org.learning.spring.weather.service.Impl;
 
-import lombok.Data;
 import org.learning.spring.weather.service.WeatherDataService;
 import org.learning.spring.weather.vo.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Data
+@Service
 public class WeatherDataServiceImpl implements WeatherDataService {
     @Autowired
     private RestTemplate restTemplate;
