d603162bd342c35ed6c0e88ce7418d6aa3c36219
chen'yang <jdiop417@gmail.com>
2018-08-10 14:34:44 +0800

    修改bug

--- msa-weather-collection-eureka/src/main/java/org/learning/spring/weather/config/QuartzConfiguration.java
+++ msa-weather-collection-eureka/src/main/java/org/learning/spring/weather/config/QuartzConfiguration.java
@@ -1,18 +1,26 @@
 package org.learning.spring.weather.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.learning.spring.weather.job.WeatherDataSyncJob;
 import org.quartz.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class QuartzConfiguration {
 
     private static final int TIME = 30 * 60;
 
     @Bean
     public JobDetail weatherDataSyncJobDetail() {
-        return JobBuilder.newJob(WeatherDataSyncJob.class).withIdentity("weatherDataSyncJob").storeDurably().build();
+        JobDetail jobDetail = null;
+        try {
+            jobDetail = JobBuilder.newJob(WeatherDataSyncJob.class).withIdentity("weatherDataSyncJob").storeDurably().build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return jobDetail;
     }
 
     @Bean
