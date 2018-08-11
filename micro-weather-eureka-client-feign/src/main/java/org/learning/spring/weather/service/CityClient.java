26b9eecdaddcd0877e084b7727ed727a90550786
chen'yang <jdiop417@gmail.com>
2018-08-11 22:30:43 +0800

    feign修改

--- micro-weather-eureka-client-feign/src/main/java/org/learning/spring/weather/service/CityClient.java
+++ micro-weather-eureka-client-feign/src/main/java/org/learning/spring/weather/service/CityClient.java
@@ -1,9 +1,11 @@
 package org.learning.spring.weather.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("sa-weather-city-eureka")
+@FeignClient("msa-weather-city-eureka")
+@Component
 public interface CityClient {
     @GetMapping("/cities")
     String listCity();
