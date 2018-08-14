b0e331ca22755599ecf107621ceeb2387ed60868
chen'yang <jdiop417@gmail.com>
2018-08-14 19:46:04 +0800

    hystrix简单实现

--- micro-weather-eureka-client-feign-hystrix/src/main/java/org/learning/spring/weather/controller/CityController.java
+++ micro-weather-eureka-client-feign-hystrix/src/main/java/org/learning/spring/weather/controller/CityController.java
@@ -1,5 +1,6 @@
 package org.learning.spring.weather.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.learning.spring.weather.service.CityClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,14 @@ public class CityController {
     @Autowired
     private CityClient cityClient;
 
+    @HystrixCommand(fallbackMethod = "defaultCities")
     @GetMapping("/cities")
     public String listCity() {
         String body = cityClient.listCity();
         return body;
+    }
+
+    public String defaultCities() {
+        return "City Data Server is down!";
     }
 }
